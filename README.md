97efa7033f285167f23d5a98db3a8815970e49d7
Yaw Nartey <yaw.nartey@bittitan.com>
2025-10-12 01:17:40 +0200

    updated: ensure proper functions are run for the user

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # go-101
 # this is a simple booking web application written in go language 
-# the whole idea and concept is to get a grasp of how the go application works for iits werkstudent interview
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -15,32 +16,51 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Printf("Get your tickets here to attend\n")
 
-	var firstName string
-	var lastName string
-	var emailAddress string
-	var userTickets uint
+	for {
 
-	//ask the user for their booking details
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+		var firstName string
+		var lastName string
+		var emailAddress string
+		var userTickets uint
 
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+		//ask the user for their booking details
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your email address: ")
-	fmt.Scan(&emailAddress)
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter the number of tickets you want to book: ")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&emailAddress)
 
-	//calculate the ramining tickets after user booked their ticket
-	remainingTickets = remainingTickets - userTickets
+		fmt.Println("Enter the number of tickets you want to book: ")
+		fmt.Scan(&userTickets)
 
-	//store users booking records
-	bookings = append(bookings, firstName+" "+lastName)
+		if userTickets <= remainingTickets {
+			remainingTickets = remainingTickets - userTickets
+			//store users booking records
+			bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, emailAddress)
-	fmt.Printf("%v tickets remaing for %v", remainingTickets, conferenceName)
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, emailAddress)
+			fmt.Printf("%v tickets remaing for %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("This is all our bookings in the application: %v\n", bookings)
+			//retrieve only the first name of the user bookings and print it out to the user
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names of bookings we have are: %v\n", firstNames)
+
+			//end the program if the remaining tickets is 0
+			if remainingTickets == 0 {
+				fmt.Println("Our Conference is fully booked. Come back next year")
+				break
+			}
+		} else {
+			fmt.Printf("We only have %v tickets remaining. You can't book %v tickets. Enter a valid ticket number less or equal to %v\n", remainingTickets, userTickets, remainingTickets)
+		}
+
+	}
+
 }
