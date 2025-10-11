d333aa467b3e5f4fc11024fd22ee0f59608be0e5
Yaw Nartey <yaw.nartey@bittitan.com>
2025-10-11 22:57:00 +0200

    updated: collected user credentails and implemented the booking logic using variables

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # go-101
+# this is a simple booking web application written in go language 
+# the whole idea and concept is to get a grasp of how the go application works for iits werkstudent interview
--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Print("hello world!")
+
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("Welcome to our %v booking application!\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Printf("Get your tickets here to attend\n")
+
+	var firstName string
+	var lastName string
+	var emailAddress string
+	var userTickets uint
+
+	//ask the user for their booking details
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&emailAddress)
+
+	fmt.Println("Enter the number of tickets you want to book: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, emailAddress)
+
+	//calculate the ramining tickets after user booked their ticket
+	remainingTickets = remainingTickets - userTickets
+	fmt.Printf("%v tickets remaing for %v", remainingTickets, conferenceName)
 }
